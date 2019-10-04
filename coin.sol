508a9bf8a01cb7755310d9a4d1d5e1571bab34c9
Niraj Kamdar <nirajkamdar27@pm.me>
2019-10-05 03:14:12 +0530

    initial message

---
+++ coin.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract BStore {
+    struct Book {
+        uint ISBN;
+        address publisher;
+        uint price;
+        string title;
+        bytes cover;
+    }
+
+    Book    
+
+}
