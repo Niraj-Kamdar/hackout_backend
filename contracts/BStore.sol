9486ef1f21d540ceafe1041beab6497dbc1680f1
Niraj Kamdar <nirajkamdar27@pm.me>
2019-10-05 16:43:32 +0530

    foreign key added

--- contracts/BStore.sol
+++ contracts/BStore.sol
@@ -2,7 +2,6 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract BStore {
     uint public bookCount;
-    bool public isAuthor;
     address public user;
     struct Book {
         uint id;
@@ -23,13 +22,12 @@ contract BStore {
     }
     function createBook(address _publisher, uint _price, string memory _title, bytes memory _cover) public{
         require(msg.sender == user);
-        require(isAuthor);
         bookCount++;
         books[bookCount] = Book(bookCount, _publisher, _price, _title, _cover);
     }
-    function addBook(uint _id, address _publisher, uint _price, string memory _title, bytes memory _cover) public{
+    function addBook(uint _id) public{
         require(msg.sender == user);
         noOfBooks[msg.sender]++;
-        ownedbooks[msg.sender][noOfBooks[msg.sender]] = Book(_id, _publisher, _price, _title, _cover);
+        ownedbooks[msg.sender][noOfBooks[msg.sender]] = books[_id];
     }
 }
