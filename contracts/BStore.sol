7bd819c17e78d58c76abff73f9ade8b72b1dac33
Niraj Kamdar <nirajkamdar27@pm.me>
2019-10-05 08:12:10 +0530

    user owned books added

--- contracts/BStore.sol
+++ contracts/BStore.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract BStore {
     uint public bookCount;
+    bool public isAuthor;
+    address public user;
     struct Book {
         uint id;
         address publisher;
@@ -11,12 +13,23 @@ contract BStore {
     }
 
     mapping(uint => Book) public books;
+    mapping(address => Book[]) public ownedbooks;
+    mapping(address => uint)public noOfBooks;
 
     constructor () public {
         bookCount = 0;
+        user = msg.sender;
+        noOfBooks[msg.sender] = 0;
     }
     function createBook(address _publisher, uint _price, string memory _title, bytes memory _cover) public{
+        require(msg.sender == user);
+        require(isAuthor);
         bookCount++;
         books[bookCount] = Book(bookCount, _publisher, _price, _title, _cover);
+    }
+    function addBook(uint _id, address _publisher, uint _price, string memory _title, bytes memory _cover) public{
+        require(msg.sender == user);
+        noOfBooks[msg.sender]++;
+        ownedbooks[msg.sender][noOfBooks[msg.sender]] = Book(_id, _publisher, _price, _title, _cover);
     }
 }
